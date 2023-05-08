2af32046f5486e65e2562e5df0c48d8c9816056b
nitzanimaven <131203076+nitzanimaven@users.noreply.github.com>
2023-05-08 14:04:00 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require('cors');
+
+// Set up your OpenAI API key and endpoint
+const configuration = new Configuration({
+    apiKey: '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',
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Create a new Express app
+const app = express();
+app.use(cors({
+    origin: '*'
+}));
+
+
+// Define a function to send a prompt to ChatGPT and get its response
+async function getChatResponse(prompt, user) {
+    try {
+       let response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `${user}: ${prompt}\nChatGPT:`,
+            max_tokens: 250,
+            temperature: 0
+        });
+
+        return response.data.choices[0].text.trim();
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// Start the server
+const server = app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
+
+// Set up socket.io
+const io = require('socket.io')(server, { cors: { origin: '*' } });
+const users = {};
+
+io.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected`);
+    socket.on('join', (username) => {
+        console.log(`User ${socket.id} joined as ${username}`);
+        users[socket.id] = username;
+        io.emit('status', `${username} joined the chat`);
+    });
+    socket.on('chat', (message) => {
+        const user = users[socket.id];
+        console.log(`User ${socket.id} sent message: ${message}`);
+        getChatResponse(message, user).then((response) => {
+            io.emit('chat', { user, message, response });
+        });
+    });
+    socket.on('disconnect', () => {
+        const username = users[socket.id];
+        console.log(`User ${socket.id} disconnected`);
+        delete users[socket.id];
+        io.emit('status', `${username} left the chat`);
+    });
+});
